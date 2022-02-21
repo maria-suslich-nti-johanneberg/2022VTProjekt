9e60ee00c600ea66ffc631ed4e43c4ab186b46b3
maria-suslich-nti-johanneberg <maria.suslich@elev.ga.ntig.se>
2022-02-21 10:05:05 +0100

    Navbar med länkar
    Navbar med länkar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 // Denna fil skall innehålla JavaScript (JS)
+
+const nav = document.querySelector('.navlinks')
+const burger = document.querySelector('.burger')
+
+function toggleMeny(){
+    nav.classList.toggle('navActive')
+    burger.classList.toggle('toggle')
+
+}
+
+burger.addEventListener('click', toggleMeny)
+
