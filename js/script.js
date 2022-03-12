42526beb79142e4f4182ada46c9fc7c05a44c20b
maria-suslich-nti-johanneberg <maria.suslich@elev.ga.ntig.se>
2022-03-12 22:31:44 +0100

    nav + header

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 // Denna fil skall innehålla JavaScript (JS)
 
-const nav = document.querySelector('.navlinks')
-const burger = document.querySelector('.burger')
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
 
-function toggleMeny(){
-    nav.classList.toggle('navActive')
-    burger.classList.toggle('toggle')
-
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
 }
-
-burger.addEventListener('click', toggleMeny)
-
