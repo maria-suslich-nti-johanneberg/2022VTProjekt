283a8e2f940701e42a9301ab08415df5b5b98c6a
maria-suslich-nti-johanneberg <maria.suslich@elev.ga.ntig.se>
2022-03-13 20:28:20 +0100

    nav+header

--- js/script.js
+++ js/script.js
@@ -12,3 +12,15 @@ function scrollFunction() {
     document.getElementById("logo").style.fontSize = "35px";
   }
 }
+
+
+const nav = document.querySelector('.navlinks')
+const burger = document.querySelector('.burger')
+
+function toggleMeny(){
+    nav.classList.toggle('navActive')
+    burger.classList.toggle('toggle')
+
+}
+
+burger.addEventListener('click', toggleMeny)
